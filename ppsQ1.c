2209d9557d8c615e632414f498b0eabbaadcda2f
KirtiSharma0 <128890117+KirtiSharma0@users.noreply.github.com>
2023-05-08 19:40:42 +0530

    Add files via upload

---
+++ ppsQ1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("NAME = KIRTI SHARMA\n");
+   printf("CLASS = SECTION D1\n");
+   printf("ROLL NO = 1/22/FET/BCS/114\n");
+
+    printf("Hello world!\n");
+    return 0;
+}
