a3acb1b1560c12ce63a2174bf743c2b108093f40
KirtiSharma0 <128890117+KirtiSharma0@users.noreply.github.com>
2023-05-08 19:58:04 +0530

    Add files via upload

---
+++ ppsQ2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int x,y,z;
+    printf("VALUE OF x = ");
+    scanf("%d",&x);
+    printf("VALUE OF y = ");
+    scanf("%d",&y);
+
+    z = x+y;
+    printf("SUM IS %d",z);
+    return 0;
+}
